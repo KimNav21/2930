219632ed47db0d031f0eb6146be296ff5184ff7e
KimNav21 <163095547+KimNav21@users.noreply.github.com>
2024-03-27 10:48:39 +0900

    Create LocalVariableDemo.java

---
+++ 실습 2/LocalVariableDemo.java
@@ -0,0 +1,18 @@
+public class LocalVariableDemo {
+    public static void main(String[] args){
+        int a;
+        double b;
+
+        // System.out.print(b);
+        //System.out.println(a + c);
+
+        int c;
+
+        //public double d = 0.0
+
+        for (int e = 0; e< 10;e++){
+            //int a = 1;
+            System.out.println(e);
+        }
+    }
+}
