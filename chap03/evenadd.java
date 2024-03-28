bdce009924d960f4953e614ccd0f313d13dd85c8
KimNav21 <163095547+KimNav21@users.noreply.github.com>
2024-03-28 18:40:08 +0900

    Create evenadd.java

---
+++ chap03/evenadd.java
@@ -0,0 +1,20 @@
+package problem;
+
+import java.util.Scanner;
+
+public class evenadd {
+	public static void main(String[] args) {
+		int n = 1, sum = 0;
+		Scanner in = new Scanner(System.in);
+
+		do {
+			System.out.print("양의 정수를 입력하세요: ");
+			n = in.nextInt();
+
+			if (n % 2 == 0)
+				sum = sum + n;
+		} while (n > 0);
+
+		System.out.println("입력한 양의 정수 중에서 짝수의 합은 " + sum);
+	}
+}
