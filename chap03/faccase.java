a47e2eeedfa8a7d1d2b26a3fd0b85a7757065663
KimNav21 <163095547+KimNav21@users.noreply.github.com>
2024-03-28 18:42:08 +0900

    Create faccase.java

---
+++ chap03/faccase.java
@@ -0,0 +1,15 @@
+package problem;
+
+public class faccase {
+	public static void main(String[] args) {
+		System.out.println(factorial(5));
+	}
+
+	static int factorial(int n) {
+		return switch (n) {
+		case 0 -> 1;
+		case 1 -> 1;
+		default -> factorial(n - 1) * n;
+		};
+	}
+}
