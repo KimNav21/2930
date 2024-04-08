ae21be32113782c021a5dd6fce8d41507b3c4dec
KimNav21 <163095547+KimNav21@users.noreply.github.com>
2024-04-08 15:04:59 +0900

    Create String4demo.java

---
+++ chap05/String4demo.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String4demo {
+
+	public static void main(String[] args) {
+		int i = 7;
+		System.out.println("Java " + i);
+		System.out.println("Java " + 7);
+		System.out.println("Java " + 7 +1 );
+
+	}
+
+}
